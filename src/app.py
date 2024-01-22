0d5093ffb2e13ff2330beb3d2085b3785c77cf4a
StoppedwummSites <150438484+StoppedwummSites@users.noreply.github.com>
2024-01-22 08:49:58 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -3,6 +3,6 @@ import PIL
 
 mainwindow = tk.Tk()
 mainwindow.title("Bud-Get")
-textlabel = tk.Label(mainwindow, text="Hello World form Bud-Get")
+textlabel = tk.Label(mainwindow, text="Hello World form Bud-Get", width=10, height=10)
 textlabel.pack()
 mainwindow.mainloop()
