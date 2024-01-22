6860911c70e808e975472fe07642c1d590c95aae
StoppedwummSites <150438484+StoppedwummSites@users.noreply.github.com>
2024-01-22 08:14:08 +0000

    Open new Window

--- src/app.py
+++ src/app.py
@@ -1,8 +1,29 @@
 import tkinter as tk
 import PIL
+import threading
+import os
+
+
+def LoadOverview():
+    window = tk.Tk()
+    window.title("Overview")
+    UBar = tk.Frame(window)
+    T = tk.Label(UBar, text="Bud-Get Overview")
+    T.pack()
+    UBar.pack()
+    window.mainloop()
+    
+
 
 mainwindow = tk.Tk()
 mainwindow.title("Bud-Get")
-textlabel = tk.Label(mainwindow, text="Hello World form Bud-Get", width=30, height=10)
-textlabel.pack()
+UpperBar = tk.Frame(mainwindow)
+Actions = tk.Frame(mainwindow)
+Title = tk.Label(UpperBar, text="Bud-Get", width=20, height=5)
+Overview = tk.Button(Actions, text="Overview", command=LoadOverview)
+Title.pack()
+UpperBar.pack()
+Overview.pack()
+Actions.pack()
+
 mainwindow.mainloop()
